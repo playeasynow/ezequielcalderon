2bb5c46b80251427b0ef16eca754a766ec588570
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-04-20 11:56:16 -0500

    first commit

--- config/website.js
+++ config/website.js
@@ -3,21 +3,21 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
+  siteTitle: 'Ezequiel Calderon Jr', // Navigation and Site Title
+  siteTitleAlt: 'Ezequiel', // Alternative Site title for SEO
+  siteTitleShort: 'Ezequiel', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://www.ezequielcalderon.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteLogo: '../public/favicons/transparent-circle-orange.png', // Used for SEO and manifest
+  siteDescription: 'Ezequiel Calderon Jr Portfolio Site',
+  author: 'Ezequiel Calderon Jr', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@playeasynow', // Twitter Username
+  ogSiteName: 'ezequiel calderon jr', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  googleAnalyticsID: '',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,7 @@ import Projects from '../views/Projects'
 import About from '../views/About'
 import Contact from '../views/Contact'
 
-import avatar from '../images/avatar.jpg'
+import avatar from '../images/ezequiel-headshot.jpg'
 
 const ProjectsWrapper = styled.div`
   ${tw`flex flex-wrap justify-between mt-8`};
@@ -62,10 +62,11 @@ const Index = () => (
     <Layout />
     <Parallax pages={5}>
       <Hero offset={0}>
-        <BigTitle>
-          Hello, <br /> I'm John Doe.
-        </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <BigTitle> Hello, I'm Ezequiel </BigTitle>
+        <Subtitle>
+          I work on software products and services with awesome people.
+          <br /> <br /> <a href="#">projects </a> | <a href="#">contact</a>
+        </Subtitle>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
@@ -121,15 +122,15 @@ const Index = () => (
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            Say <a href="mailto:ezequielcalderonjr@gmail.com">Hi</a> or find me on other platforms:{' '}
+            <a href="https://github.com/playeasynow">GitHub</a> &{' '}
+            <a href="https://www.instagram.com/playeasynow/">Instagram</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
+          &copy; 2019 by Ezequiel Calderon Jr{''} <br />
+          {/* <a href="https://github.com/playeasynow">Github Repository</a>. */}
+          {/* Made by{' '} <a href="https://www.ezequielcalderon.com">Ezequiel Calderon Jr</a>. */}
         </Footer>
       </Contact>
     </Parallax>
